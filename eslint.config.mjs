5a54366b5566c9b11ce460668d82d5670cc5fffb
DiegoBg09 <diego.bgtlv26@gmail.com>
2025-05-26 11:18:13 -0500

    [ INIT ]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,50 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// Plugins y configuraciones
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
 
-export default eslintConfig;
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}']
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-indent': [2, 2],
+      'array-bracket-spacing': [2, 'always'],
+      'arrow-body-style': ['error', 'as-needed'],
+      'comma-dangle': [2, 'never'],
+      'eol-last': 2,
+      'jsx-quotes': ['error', 'prefer-single'],
+      'quotes': [2, 'single', { avoidEscape: true }],
+      'object-curly-spacing': ['error', 'always'],
+      'indent': [2, 2, { SwitchCase: 1 }],
+      'key-spacing': ['error', { align: 'colon' }],
+      'semi': [2, 'never'],
+      'no-trailing-spaces': 2
+    }
+  }
+]
